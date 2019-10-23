385b67f1302b4b3abeb7d607f6ef872049c0d75a
toyosi12 <toyosioyelayo@gmail.com>
2019-10-23 18:12:09 +0100

    changed mobile layout

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,7 @@ class Item extends Component {
     state = {  }
     render() { 
         return ( 
-                <div className="col-3">
+                <div className="col-md-3">
                         <div className="card">
                             <img src={require('../components/assets/images/bg.jpg')} className="card-img-top" />
                             <div className="card-body">
